b3fc4a0f2ec812f524af077cf4416a99cb6b19c0
Matt Innes <matt@madeye.com>
2018-03-07 13:41:25 +0000

    Fixed codacy errors

--- src/main/java/uk/gov/ons/ctp/response/collection/exercise/representation/SampleUnitValidationErrorDTO.java
+++ src/main/java/uk/gov/ons/ctp/response/collection/exercise/representation/SampleUnitValidationErrorDTO.java
@@ -2,8 +2,14 @@ package uk.gov.ons.ctp.response.collection.exercise.representation;
 
 import lombok.Data;
 
+/**
+ * DTO for collection exercise validation errors
+ */
 @Data
 public class SampleUnitValidationErrorDTO {
+    /**
+     * An enum outlining the different types of validation errors that can affect sample units
+     */
     public enum ValidationError { MISSING_COLLECTION_INSTRUMENT, MISSING_PARTY }
 
     private String sampleUnitRef;
